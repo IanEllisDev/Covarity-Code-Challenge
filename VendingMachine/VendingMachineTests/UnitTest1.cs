3f91578f48897786db80d32811bbb229a3d93ab6
Ian <ianellisdev@gmail.com>
2016-07-12 21:23:48 -0400

    Initial project setup

---
+++ VendingMachine/VendingMachineTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Pass()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void Fail()
+        {
+            Assert.AreEqual(2, 1);
+        }
+    }
+}
