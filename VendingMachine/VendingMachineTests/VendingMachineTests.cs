d0eae984e3a907fef6565600a4e9a607cfc16d28
Ian <ianellisdev@gmail.com>
2016-07-18 21:40:03 -0400

    Test adjustments

--- VendingMachine/VendingMachineTests/VendingMachineTests.cs
+++ VendingMachine/VendingMachineTests/VendingMachineTests.cs
@@ -23,19 +23,33 @@ namespace VendingMachineTests
         [TestMethod]
         public void VendingMachine_InitializeWithValues_ValuesAreInitialized()
         {
+            var ones = 5;
+            var tens = 100;
+            var fives = 62;
             var mockData = new Dictionary<Denomination, int>() {
-                { Denomination.One, 5 },
-                { Denomination.Ten, 100 },
-                { Denomination.Five, 62 },
+                { Denomination.One, ones },
+                { Denomination.Ten, tens},
+                { Denomination.Five, fives },
             };
 
             var machine = new VendingMachine(mockData);
             Assert.IsNotNull(machine.Money);
             Assert.IsNotNull(machine.UserInputtedMoney);
             Assert.IsNotNull(machine.Money.DenominiationCounts);
-            Assert.IsNotNull(machine.Money.DenominiationCounts);
-            Assert.AreEqual(0, machine.Money.DenominiationCounts.Count);
-            Assert.AreEqual(0, machine.Money.DenominiationCounts.Count);
+            Assert.IsNotNull(machine.UserInputtedMoney.DenominiationCounts);
+            Assert.AreEqual(3, machine.Money.DenominiationCounts.Count);
+            Assert.AreEqual(0, machine.UserInputtedMoney.DenominiationCounts.Count);
+
+            var denominizations = machine.Money.DenominiationCounts;
+            Assert.IsTrue(denominizations.ContainsKey(Denomination.One));
+            Assert.IsFalse(denominizations.ContainsKey(Denomination.Two));
+            Assert.IsTrue(denominizations.ContainsKey(Denomination.Five));
+            Assert.IsTrue(denominizations.ContainsKey(Denomination.Ten));
+            Assert.AreEqual(ones, denominizations[Denomination.One]);
+            Assert.AreEqual(fives, denominizations[Denomination.Five]);
+            Assert.AreEqual(tens, denominizations[Denomination.Ten]);
+
         }
+        
     }
 }
